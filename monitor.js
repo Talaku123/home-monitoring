b8addb904d6c3938710300d083ff632558dad6aa
Talaku123 <69880305+Talaku123@users.noreply.github.com>
2024-03-05 00:25:42 -0500

    Add files via upload

---
+++ monitor.js
@@ -0,0 +1,23 @@
+
+
+
+
+// let counter = 65;
+
+// while (counter = 1) {
+//   console.log(counter);
+//   counter += 1;
+
+//   if (counter > 2) {
+//     break;
+//   }
+// }
+// let counter = 5;
+// while (counter < 10) {
+//   console.log(counter);
+//   counter = counter + 1;
+// }
+var myCollapsible = document.getElementById('myCollapsible')
+myCollapsible.addEventListener('hidden.bs.collapse', function () {
+  // do something...
+})
